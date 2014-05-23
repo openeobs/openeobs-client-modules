a273184d83aadc65e2abecd91958d63058d2f971
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-05-23 15:04:53 +0100

    [IMP][TASK 939] T4Clinical Config - LTH

---
+++ t4clinical_lth/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'T4 Clinical LTH Configuration',
+    'version': '0.1',
+    'category': 'Clinical',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """    """,
+    'author': 'Tactix4',
+    'website': 'http://www.tactix4.com/',
+    'depends': ['t4clinical_ui'],
+    'data': ['lth_master_data.xml'],
+    'qweb': ['static/src/xml/t4clinical_lth.xml'],
+    'css': ['static/src/css/t4clinical_lth.css'],
+    'application': True,
+    'installable': True,
+    'active': False,
+}
