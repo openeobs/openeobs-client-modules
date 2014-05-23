e8fb67732c3af7c2e489a2b2d3a298844c7d3446
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-05-23 17:47:50 +0100

    [IMP][TASK 941] T4Clinical Config - LDH

---
+++ t4clinical_ldh/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'T4 Clinical LDH Configuration',
+    'version': '0.1',
+    'category': 'Clinical',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """    """,
+    'author': 'Tactix4',
+    'website': 'http://www.tactix4.com/',
+    'depends': ['t4clinical_ui'],
+    'data': ['security/ir.model.access.csv',
+             'ldh_master_data.xml',
+             't4clinical_ldh_view.xml'],
+    'qweb': ['static/src/xml/t4clinical_ldh.xml'],
+    'css': ['static/src/css/t4clinical_ldh.css'],
+    'application': True,
+    'installable': True,
+    'active': False,
+}
