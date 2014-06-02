e61636f1d150e2258acd69ebb6064f51060b097f
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-06-02 15:13:01 +0100

    [IMP][TASK 983] LDH policy Clerking + Review Test

--- t4clinical_ldh/tests/test_ldh_policy.py
+++ t4clinical_ldh/tests/test_ldh_policy.py
@@ -2,112 +2,79 @@ from openerp.tests import common
 from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT as DTF
 from faker import Faker
 import logging
+from openerp.addons.t4clinical_activity_types.tests.test_scenario import ActivityTypesTest
 
 _logger = logging.getLogger(__name__)
 
 faker = Faker()
 
 
-class TestLdhPolicy(common.SingleTransactionCase):
+class TestLdhPolicy(ActivityTypesTest):
 
     def setUp(self):
-        global cr, uid
-        global patient_pool, activity_pool, register_pool, admit_pool, placement_pool, location_pool
-        global api_pool
+        global cr, uid, \
+               register_pool, patient_pool, admit_pool, activity_pool, transfer_pool, ews_pool, \
+               activity_id, api_pool, location_pool, pos_pool, user_pool, imd_pool, discharge_pool, \
+               device_connect_pool, device_disconnect_pool, partner_pool, height_pool, blood_sugar_pool, \
+               blood_product_pool, weight_pool, stools_pool, gcs_pool, vips_pool, o2target_pool, o2target_activity_pool
 
         cr, uid = self.cr, self.uid
 
-        patient_pool = self.registry('t4.clinical.patient')
-        activity_pool = self.registry('t4.activity')
         register_pool = self.registry('t4.clinical.adt.patient.register')
+        patient_pool = self.registry('t4.clinical.patient')
         admit_pool = self.registry('t4.clinical.adt.patient.admit')
+        discharge_pool = self.registry('t4.clinical.patient.discharge')
         activity_pool = self.registry('t4.activity')
-        placement_pool = self.registry('t4.clinical.patient.placement')
-        location_pool = self.registry('t4.clinical.location')
+        transfer_pool = self.registry('t4.clinical.adt.patient.transfer')
+        ews_pool = self.registry('t4.clinical.patient.observation.ews')
+        height_pool = self.registry('t4.clinical.patient.observation.height')
+        weight_pool = self.registry('t4.clinical.patient.observation.weight')
+        blood_sugar_pool = self.registry('t4.clinical.patient.observation.blood_sugar')
+        blood_product_pool = self.registry('t4.clinical.patient.observation.blood_product')
+        stools_pool = self.registry('t4.clinical.patient.observation.stools')
+        gcs_pool = self.registry('t4.clinical.patient.observation.gcs')
+        vips_pool = self.registry('t4.clinical.patient.observation.vips')
         api_pool = self.registry('t4.clinical.api')
+        location_pool = self.registry('t4.clinical.location')
+        pos_pool = self.registry('t4.clinical.pos')
+        user_pool = self.registry('res.users')
+        partner_pool = self.registry('res.partner')
+        imd_pool = self.registry('ir.model.data')
+        device_connect_pool = self.registry('t4.clinical.device.connect')
+        device_disconnect_pool = self.registry('t4.clinical.device.disconnect')
+        o2target_pool = self.registry('t4.clinical.o2level')
+        o2target_activity_pool = self.registry('t4.clinical.patient.o2target')
 
         super(TestLdhPolicy, self).setUp()
 
-    def xml2db_id(self, xmlid):
-        imd_pool = self.registry('ir.model.data')
-        imd_id = imd_pool.search(self.cr, self.uid, [('name', '=', xmlid)])
-        db_id = imd_id and imd_pool.browse(self.cr, self.uid, imd_id[0]).res_id or False
-        return db_id
+    def test_ldh_clerking_and_review_policy(self):
+        # environment
+        pos1_env = self.create_pos_environment()
+        # register
+        [self.adt_patient_register(env=pos1_env) for i in range(5)]
 
-    def test_ldh_policy(self):
-        global cr, uid
-        global patient_pool, activity_pool, ews_pool, register_pool, admit_pool, placement_pool, location_pool
-        global faker, api_pool
+        # admit
+        [self.adt_patient_admit(data_vals={'other_identifier': other_identifier}, env=pos1_env) for other_identifier in pos1_env['other_identifiers']]
 
-        adt_uid = self.xml2db_id("t4c_ldh_adt_user")
+        # placements
+        [self.patient_placement(data_vals={'patient_id': patient_id}, env=pos1_env) for patient_id in pos1_env['patient_ids']]
 
-        gender = faker.random_element(array=('M', 'F'))
-        patient_data = {
-            'family_name': faker.last_name(),
-            'other_identifier': str(faker.random_int(min=1001, max=9999)),
-            'dob': faker.date_time_between(start_date="-80y", end_date="-10y").strftime(DTF),
-            'gender': gender,
-            'sex': gender,
-            'given_name': faker.first_name()
-        }
-        reg_activity_id = register_pool.create_activity(cr, adt_uid, {}, patient_data)
-        self.assertTrue(reg_activity_id, msg='Error trying to register patient')
-        print "TEST - setting up LDH policy tests - " + "Patient registered."
+        for patient_id in pos1_env['patient_ids']:
+            clerking_ids = activity_pool.search(cr, uid, [
+                ('patient_id', '=', patient_id),
+                ('data_model', '=', 't4.clinical.ldh.patient.clerking'),
+                ('state', 'not in', ['completed', 'cancelled'])])
+            self.assertTrue(clerking_ids, msg='Clerking activity not created')
+            activity_pool.complete(cr, uid, clerking_ids[0])
 
-        patient_domain = [(k, '=', v) for k, v in patient_data.iteritems()]
-        patient_id = patient_pool.search(cr, adt_uid, patient_domain)
-        self.assertTrue(patient_id, msg='Patient not created')
-        patient_id = patient_id[0]
-        admit_data = {
-            'code': str(faker.random_int(min=10001, max=99999)),
-            'other_identifier': patient_data['other_identifier'],
-            'location': faker.random_element(array=('EAU', 'AE')),
-            'start_date': faker.date_time_between(start_date="-1w", end_date="-1h").strftime(DTF)
-        }
-        admit_activity_id = admit_pool.create_activity(cr, adt_uid, {}, admit_data)
-        self.assertTrue(admit_activity_id, msg='Error trying to admit patient')
-        activity_pool.complete(cr, adt_uid, admit_activity_id)
-        print "TEST - setting up LDH policy tests - " + "Patient admitted."
-        available_bed_location_ids = location_pool.get_available_location_ids(cr, uid, ['bed'])
-        if admit_data['location'] == 'EAU':
-            wm_uid = self.xml2db_id("t4c_ldh_ward_manager_winifred_user")
-            hca_uid = self.xml2db_id("t4c_ldh_hca_harold_user")
-            nur_uid = self.xml2db_id("t4c_ldh_nurse_norah_user")
-            doc_uid = self.xml2db_id("t4c_ldh_doctor_dave_user")
-        else:
-            wm_uid = self.xml2db_id("t4c_ldh_ward_manager_whitney_user")
-            hca_uid = self.xml2db_id("t4c_ldh_hca_hannah_user")
-            nur_uid = self.xml2db_id("t4c_ldh_nurse_nathan_user")
-            doc_uid = self.xml2db_id("t4c_ldh_doctor_davina_user")
-        location_ids = location_pool.search(cr, uid, [
-                ('parent_id.code', '=', admit_data['location']),
-                ('id', 'in', available_bed_location_ids)])
-        if not location_ids:
-            _logger.warning("No available locations found for parent location %s" % admit_data['location'])
-            return
-        location_id = location_ids[0]
-        placement_activity_ids = placement_pool.search(cr, uid, [('patient_id', '=', patient_id)])
-        self.assertTrue(placement_activity_ids, msg='Placement activity not created')
-        placement_id = placement_pool.read(cr, uid, placement_activity_ids[0], ['activity_id'])
-        placement_activity_id = placement_id['activity_id'][0]
+        print "TEST - LDH policy tests - " + "Patient clerkings completed."
 
-        activity_pool.submit(cr, wm_uid, placement_activity_id, {'location_id': location_id})
+        for patient_id in pos1_env['patient_ids']:
+            review_ids = activity_pool.search(cr, uid, [
+                ('patient_id', '=', patient_id),
+                ('data_model', '=', 't4.clinical.ldh.patient.review'),
+                ('state', 'not in', ['completed', 'cancelled'])])
+            self.assertTrue(review_ids, msg='Review activity not created')
+            activity_pool.complete(cr, uid, review_ids[0])
 
-        activity_pool.complete(cr, wm_uid, placement_activity_id)
-        print "TEST - LDH policy tests - " + "Patient placement completed."
-
-        clerking_ids = activity_pool.search(cr, hca_uid, [
-            ('patient_id', '=', patient_id),
-            ('data_model', '=', 't4.clinical.ldh.patient.clerking'),
-            ('state', 'not in', ['completed', 'cancelled'])])
-        self.assertTrue(clerking_ids, msg='Clerking activity not created')
-        activity_pool.complete(cr, hca_uid, clerking_ids[0])
-        print "TEST - LDH policy tests - " + "Patient clerking completed."
-
-        review_ids = activity_pool.search(cr, doc_uid, [
-            ('patient_id', '=', patient_id),
-            ('data_model', '=', 't4.clinical.ldh.patient.review'),
-            ('state', 'not in', ['completed', 'cancelled'])])
-        self.assertTrue(review_ids, msg='Review activity not created')
-        activity_pool.complete(cr, doc_uid, review_ids[0])
-        print "TEST - LDH policy tests - " + "Patient review completed."
+        print "TEST - LDH policy tests - " + "Patient reviews completed."
