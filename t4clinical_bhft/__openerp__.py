acee2845a38c5f3ce290e4ce3dab3cf2ac21b086
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-09-22 14:26:08 +0100

    initial commit

---
+++ t4clinical_bhft/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'T4 Clinical BHFT Configuration',
+    'version': '0.1',
+    'category': 'Clinical',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """    """,
+    'author': 'Tactix4',
+    'website': 'http://www.tactix4.com/',
+    'depends': ['t4clinical_ui'],
+    'data': [],
+    'qweb': [],
+    'application': True,
+    'installable': True,
+    'active': False,
+}
