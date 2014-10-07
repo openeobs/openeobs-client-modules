610243cb5aa8cb63ad9433c659ddbdc9e74ca97c
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-10-07 17:02:14 +0100

    [IMP] Refactor. mobile_frontend is now nh_eobs_mobile

--- nh_clinical_btuh/__openerp__.py
+++ nh_clinical_btuh/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 {
-    'name': 'NH Clinical BTUH Configuration',
+    'name': 'Open e-Obs BTUH Configuration',
     'version': '0.1',
     'category': 'Clinical',
     'license': 'AGPL-3',
@@ -8,7 +8,7 @@
     'description': """    """,
     'author': 'Neova Health',
     'website': 'http://www.neovahealth.co.uk/',
-    'depends': ['mobile_frontend'],
+    'depends': ['nh_eobs_mobile'],
     'data': ['btuh_master_data.xml',
              'btuh_view.xml'],
     'qweb': ['static/src/xml/nh_clinical_btuh.xml'],
