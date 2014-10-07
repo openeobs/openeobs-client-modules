7b0069b600329e027d0aebcc0dad256640c7082e
Joel Ortiz <joelortiz@neovahealth.co.uk>
2014-10-07 17:02:15 +0100

    [IMP] Refactor. mobile_frontend is now nh_eobs_mobile

--- nh_clinical_lth/__openerp__.py
+++ nh_clinical_lth/__openerp__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 {
-    'name': 'NH Clinical LTH Configuration',
+    'name': 'Open e-Obs LTH Configuration',
     'version': '0.1',
     'category': 'Clinical',
     'license': 'AGPL-3',
@@ -8,7 +8,7 @@
     'description': """    """,
     'author': 'Neova Health',
     'website': 'http://www.neovahealth.co.uk/',
-    'depends': ['mobile_frontend'],
+    'depends': ['nh_eobs_mobile'],
     'data': ['lth_master_data.xml'],
     'qweb': ['static/src/xml/nh_clinical_lth.xml'],
     'application': True,
