b4954dca52ffd97791cd067c3ab9f4ce6ebe97b0
Colin Wren <colinwren@nhtek.io>
2016-08-16 21:36:29 +0000

    Reverting back to old way as will just remove backslashes from directory name

--- nh_deployment/source/bcp_rsync.sh
+++ nh_deployment/source/bcp_rsync.sh
@@ -120,7 +120,7 @@ dryrunDestination() {
 	for K in "${!REMOTEDIRMAP[@]}"
 	do
 	    echo "INFO: rsync for $K started at $DATE"
-	    find ${LOCAL_RSYNC_DIR} -type f -a -iname "${K}_*.pdf" -exec basename {} \; > files_to_copy && rsync --dry-run -rv --files-from=files_to_copy ${LOCAL_RSYNC_DIR} $LOCAL_MOUNT_POINT/$REMOTEDIRMAP[$K]
+	    find ${LOCAL_RSYNC_DIR} -type f -a -iname "${K}_*.pdf" -exec basename {} \; > files_to_copy && rsync --dry-run -rv --files-from=files_to_copy ${LOCAL_RSYNC_DIR} "${LOCAL_MOUNT_POINT}/${REMOTEDIRMAP[$K]}"
 	    checkErrors $? "ERROR: rsync errored for some reason. Return code was: $?."
 	done
 }
@@ -132,7 +132,7 @@ rsyncDestination() {
 	for K in "${!REMOTEDIRMAP[@]}"
 	do
 	    echo "INFO: rsync for $K started at $DATE"
-	    find ${LOCAL_RSYNC_DIR} -type f -a -iname "${K}_*.pdf" -exec basename {} \; > files_to_copy && rsync -rv --files-from=files_to_copy ${LOCAL_RSYNC_DIR} $LOCAL_MOUNT_POINT/$REMOTEDIRMAP[$K]
+	    find ${LOCAL_RSYNC_DIR} -type f -a -iname "${K}_*.pdf" -exec basename {} \; > files_to_copy && rsync -rv --files-from=files_to_copy ${LOCAL_RSYNC_DIR} "${LOCAL_MOUNT_POINT}/${REMOTEDIRMAP[$K]}"
 	    checkErrors $? "ERROR: rsync errored for some reason. Return code was: $?."
 	done
 }
