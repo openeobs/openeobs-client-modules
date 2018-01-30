397f159114be53c67e9fb53a009eed6a52407084
Colin Wren <colin.wren@bjss.com>
2018-01-30 17:22:24 +0000

    - Initial commit

---
+++ uservoice/static/src/js/uservoice-widget.js
@@ -0,0 +1,14 @@
+UserVoice=window.UserVoice||[];
+(function(){
+  var uv=document.createElement('script');
+  uv.type='text/javascript';
+  uv.async=true;
+  uv.src='//widget.uservoice.com/xiXrGR5j7JSb6wqDtOQJw.js';
+  var s=document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(uv,s)
+})();
+UserVoice.push(['addTrigger', {}]);
+UserVoice.push(['set', {
+    'screenshot_enabled': false,
+    'forum_id': 909394
+}]);
