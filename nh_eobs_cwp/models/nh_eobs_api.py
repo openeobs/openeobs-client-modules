8b8d64ebadfe18d4e419b238cfdf5f298317a6e5
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2018-05-02 13:29:44 +0100

    WIP, adding therapeutic obs form.

---
+++ nh_eobs_cwp/models/nh_eobs_api.py
@@ -0,0 +1,51 @@
+from openerp import models, api
+
+
+class NhEobsApi(models.Model):
+
+    _inherit = 'nh.eobs.api'
+
+    @api.model
+    def get_active_observations(self, patient_id):
+        # Check for obs stop.
+        if not super(NhEobsApi, self).get_active_observations(patient_id):
+            return []
+        active_observations = [
+            {
+                'type': 'ews',
+                'name': 'NEWS'
+            },
+            {
+                'type': 'therapeutic',
+                'name': 'Therapeutic'
+            },
+            {
+                'type': 'blood_glucose',
+                'name': 'Blood Glucose'
+            },
+            {
+                'type': 'blood_product',
+                'name': 'Blood Product'
+            },
+            {
+                'type': 'height',
+                'name': 'Height'
+            },
+            {
+                'type': 'neurological',
+                'name': 'Neurological'
+            },
+            {
+                'type': 'pbp',
+                'name': 'Postural Blood Pressure'
+            },
+            {
+                'type': 'weight',
+                'name': 'Weight'
+            },
+        ]
+        if not self.user_allocated_to_patient(patient_id):
+            active_observations = filter(
+                lambda active_observation: active_observation['type'] != 'ews',
+                active_observations)
+        return active_observations
